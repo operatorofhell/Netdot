cb4ca86374e4c7382c2585301992de2248c003fb
Moritz Kraus <moritz.kraus@muellergroup.com>
2016-03-22 16:17:36 +0100

    added defaults for ISC DHCP PEER

--- lib/Netdot/Model/DhcpPeer.pm
+++ lib/Netdot/Model/DhcpPeer.pm
@@ -5,7 +5,55 @@ use warnings;
 use strict;
 
 my $logger = Netdot->log->get_logger('Netdot::Model::DHCP');
+############################################################################
 
+=head2 insert - Insert a new ISC DHCP  Peer group 
+
+    We override the insert method for extra functionality.
+
+ Args: 
+        name
+        hba
+        max_response_delay
+        max_unacked_updates
+        mclt
+        load_balance_max_seconds
+        port
+        primary_peer                link to RR
+        secondary_peer              link to RR
+        split
+        auto_partner_down
+  Returns: 
+    DhcpPeer object
+  Examples:
+    my $pg = DhcpPeer->insert({name=>'PG_TEST'});
+    
+=cut
+sub insert {
+    my ($class, $argv) = @_;
+    $class->throw_fatal("Model::Zone::insert: Missing required arguments")
+	unless ( $argv->{name} );
+    
+    my %state = (
+        
+        name                        => $argv->{name},
+        primary_peer                => $argv->{primary_peer},
+        secondary_peer              => $argv->{secondary_peer},
+        hba                         => $argv->{hba},
+        max_response_delay          => $argv->{max_response_delay}          ||$class->config->get('DEFAULT_DHCP_MAX_RESPONSE_DELAY'),
+        max_unacked_updates         => $argv->{max_unacked_updates}         ||$class->config->get('DEFAULT_DHCP_MAX_UNACKED_UPDATES'),
+        mclt                        => $argv->{mclt}                        ||$class->config->get('DEFAULT_DHCP_MCLT'),
+        load_balance_max_seconds    => $argv->{load_balance_max_seconds}    ||$class->config->get('DEFAULT_DHCP_LOAD_BALANCE_MAX_SECONDS'),
+        port                        => $argv->{port}                        ||$class->config->get('DEFAULT_DHCP_PORT'),
+        split                       => $argv->{split}                       ||$class->config->get('DEFAULT_DHCP_SPLIT'),
+        auto_partner_down           => $argv->{auto_partner_down}           ||$class->config->get('DEFAULT_DHCP_AUTO_PARTNER_DOWN'),
+
+    );
+
+	my $newpg = $class->SUPER::insert( \%state );
+    
+    return $newpg
+}
 ############################################################################
 
 =head2 generate_config -  Returns the config  (ISC DHCPD format)
